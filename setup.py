c9a4048226e7c5f90efbf8c34a5ea0f3e06dcc60
Rory McCann <rory@geofabrik.de>
2016-03-11 14:41:23 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pgindexrebuild",
-    version="0.2.0",
+    version="0.3.0",
     author="Rory McCann",
     author_email="rory@geofabrik.de",
     py_modules=['pgindexrebuild'],
