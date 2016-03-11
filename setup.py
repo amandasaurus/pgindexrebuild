b8faab276718abd8d98dff2e0a88f6aa916ebf83
Rory McCann <rory@geofabrik.de>
2016-03-11 13:00:14 +0100

    feat: Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="pgindexrebuild",
+    version="0.2.0",
+    author="Rory McCann",
+    author_email="rory@geofabrik.de",
+    py_modules=['pgindexrebuild'],
+    platforms=['any',],
+    install_requires=[
+        'psycopg2',
+        ],
+    entry_points={
+        'console_scripts': [
+            'pgindexrebuild = pgindexrebuild:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+    ],
+)
