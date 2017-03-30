9837f2a07551d16c798e94aa3f0771114711d821
Rory McCann <rory@geofabrik.de>
2017-03-30 14:24:41 +0200

    v0.15.0rc1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pgindexrebuild",
-    version="0.14.0",
+    version="0.15.0rc1",
     author="Rory McCann",
     author_email="rory@geofabrik.de",
     py_modules=['pgindexrebuild'],
